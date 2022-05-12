31fbdfa8d8109c79567ce87846b9e1a36111378c
tutiya_kousetsu <kousetsu.sora0111@gmail.com>
2022-05-12 11:17:27 +0900

    Revert "?"
    This reverts commit 019be090e0043fe09851b3c3ccecba0ba75364d9.

--- GamePlayScene.h
+++ GamePlayScene.h
@@ -3,27 +3,18 @@
 #include "Object3d.h"
 #include "Sprite.h"
 #include "DebugCamera.h"
-#include <DirectXMath.h>
-
+#include "DirectXCommon.h"
+#include "Fbx_Object3d.h"
 /// <summary>
 /// ゲームプレイシーン
 /// </summary>
 class GamePlayScene
 {
-private: // エイリアス
-// Microsoft::WRL::を省略
-	template <class T> using ComPtr = Microsoft::WRL::ComPtr<T>;
-	// DirectX::を省略
-	using XMFLOAT2 = DirectX::XMFLOAT2;
-	using XMFLOAT3 = DirectX::XMFLOAT3;
-	using XMFLOAT4 = DirectX::XMFLOAT4;
-	using XMMATRIX = DirectX::XMMATRIX;
-
 public:
 	/// <summary>
 	/// 初期化
 	/// </summary>
-	void Initialize();
+	void Initialize(DirectXCommon* dxCommon);
 
 	void Finalize();
 
@@ -35,19 +26,17 @@ public:
 	/// <summary>
 	/// 描画
 	/// </summary>
-	void Draw();
+	void Draw(DirectXCommon* dxCommon);
 
 private:
+	
 	Sprite* sprite = nullptr;
 	Model* modelPost = nullptr;
 	Object3d* objPost = nullptr;
 	DebugCamera* camera = nullptr;
+	DirectXCommon* dxCommon;
 
-	float speed = 0.75f;
-	float t = 0.01f;
-	float speed2 = 0.75f;
-	double g = 0.0f;
-	int Flag = 0;
-	XMFLOAT3 playerPos;
+	Fbx_Model* fbxmodel1 = nullptr;
+	Fbx_Object3d* fbxobject1 = nullptr;
 };
 

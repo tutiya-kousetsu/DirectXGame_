e7efd451d6255fe89147f0494936714b2639ec9b
tutiya_kousetsu <kousetsu.sora0111@gmail.com>
2023-06-28 12:18:38 +0900

    playerHPの間隔の変更

--- SourceCode/application/player/PlayerLife.cpp
+++ SourceCode/application/player/PlayerLife.cpp
@@ -3,19 +3,19 @@
 void PlayerLife::Initialize()
 {
 	Sprite::LoadTexture(2, L"Resources/HP.png");
-	lifeSp[0].reset(Sprite::Create(2, { 1020,656 }));
+	lifeSp[0].reset(Sprite::Create(2, { 950,656 }));
 	Sprite::LoadTexture(3, L"Resources/HP.png");
-	lifeSp[1].reset(Sprite::Create(3, { 1052,656 }));
+	lifeSp[1].reset(Sprite::Create(3, { 992,656 }));
 	Sprite::LoadTexture(4, L"Resources/HP.png");
-	lifeSp[2].reset(Sprite::Create(4, { 1084,656 }));
+	lifeSp[2].reset(Sprite::Create(4, { 1034,656 }));
 	Sprite::LoadTexture(5, L"Resources/HP.png");
-	lifeSp[3].reset(Sprite::Create(5, { 1116,656 }));
+	lifeSp[3].reset(Sprite::Create(5, { 1076,656 }));
 	Sprite::LoadTexture(6, L"Resources/HP.png");
-	lifeSp[4].reset(Sprite::Create(6, { 1148,656 }));
+	lifeSp[4].reset(Sprite::Create(6, { 1118,656 }));
 	Sprite::LoadTexture(7, L"Resources/HP.png");
-	lifeSp[5].reset(Sprite::Create(7, { 1180,656 }));
+	lifeSp[5].reset(Sprite::Create(7, { 1160,656 }));
 	Sprite::LoadTexture(8, L"Resources/HP.png");
-	lifeSp[6].reset(Sprite::Create(8, { 1212,656 }));
+	lifeSp[6].reset(Sprite::Create(8, { 1202,656 }));
 	Sprite::LoadTexture(9, L"Resources/HP.png");
 	lifeSp[7].reset(Sprite::Create(9, { 1244,656 }));
 
--- SourceCode/application/scene/TitleScene.cpp
+++ SourceCode/application/scene/TitleScene.cpp
@@ -35,7 +35,7 @@ void TitleScene::Initialize(DirectXCommon* dxCommon)
 	//床のオブジェクト生成
 	floor.reset(TouchableObject::Create(Model::CreateFromOBJ("FloorBox")));
 	floor->SetScale({ 20.f, 5.0f, 20.f });
- 	floor->SetPosition({ 0,-18.5f,0 });
+	floor->SetPosition({ 0,-18.5f,0 });
 
 	//データ読み込み
 	skyObj = Object3d::Create();
